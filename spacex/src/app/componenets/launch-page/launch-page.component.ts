6b13f9fece8efafab8a667a914c8546358d70fe4
Mariusz Briegmann <mariusz.briegmann@britenet.com.pl>
2022-08-18 21:38:54 +0200

    [BS-03] On push strategy

--- spacex/src/app/componenets/launch-page/launch-page.component.ts
+++ spacex/src/app/componenets/launch-page/launch-page.component.ts
@@ -1,24 +1,14 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  HostListener,
-  Input,
-  OnInit,
-  SimpleChanges,
-  ViewChild,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Location } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { ILaunches } from 'src/app/interfaces/launches';
 import { SpinnerService } from '../../services/spinner/spinner.service';
-import {BehaviorSubject, interval, Observable} from 'rxjs';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, interval } from 'rxjs';
 import { ITimeline } from '../../interfaces/timeline';
 import { MatStepper } from '@angular/material/stepper';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-launch-page',
@@ -30,7 +20,7 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
       useValue: { displayDefaultIndicatorType: false },
     },
   ],
-  //changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LaunchPageComponent implements OnInit {
   constructor(
@@ -38,33 +28,27 @@ export class LaunchPageComponent implements OnInit {
     private route: ActivatedRoute,
     private httpClient: HttpClient,
     private sanitizer: DomSanitizer,
-    private location: Location,
-    private _formBuilder: FormBuilder
+    private location: Location
   ) {}
-
-  @Input() public groundProgressbarValue = 100;
-  @Input() public airProgressbarValue = 100;
-  @Input() public secTillLiftoff: number = 0;
-  @Input() public secInAir: number = 0;
 
   @ViewChild('stepperGround') private stepperGround: MatStepper;
   @ViewChild('stepperAir') private stepperAir: MatStepper;
+
+  public groundProgressbarValue: BehaviorSubject<number> = new BehaviorSubject(100);
+  public airProgressbarValue: BehaviorSubject<number> = new BehaviorSubject(100);
+
+  public secTillLiftoff: number = 0;
+  public secInAir: number = 0;
 
   public launch: ILaunches;
   public images: Array<any> = [];
   public widthPX: number = 0;
 
+  public containTimeLine: boolean = false;
   public groundTimeline: ITimeline[] = new Array();
   public airTimeline: ITimeline[] = new Array();
 
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-
+  // Offset as number 1
   private offset: number = 1;
 
   @HostListener('window:resize', ['$event'])
@@ -77,10 +61,7 @@ export class LaunchPageComponent implements OnInit {
   }
 
   public renderYT(URL: any): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(
-      ''
-      // 'https://www.youtube.com/embed' + URL + '?autoplay=1&mute=1'
-    );
+    return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + URL + '?autoplay=1&mute=1');
   }
 
   public imagesToArray(images: []): void {
@@ -89,46 +70,50 @@ export class LaunchPageComponent implements OnInit {
     });
   }
 
-  public timelineStepperSetter(timeline: ITimeline[], startFrom: number, currentSeconds: number, stepper: MatStepper): void {
+  public timelineStepperSetter(
+    timeline: ITimeline[],
+    startFrom: number,
+    currentSeconds: number,
+    stepper: MatStepper
+  ): void {
     timeline.forEach((element, index) => {
       if (startFrom - currentSeconds == element.seconds) {
         element.isVisible = true;
         if (index === 0) {
           timeline[index + this.offset].isVisible = true;
         } else {
-          timeline[index + 1].isVisible = true;
+          timeline[index + this.offset].isVisible = true;
           setTimeout(() => {
             timeline[index - this.offset].isVisible = false;
-            stepper.selectedIndex = 1;
-          },100);
-          timeline[index + 2].isVisible = true;
+            stepper.selectedIndex = this.offset;
+          }, 100);
+          timeline[index + this.offset + this.offset].isVisible = true;
         }
       }
     });
   }
 
-  public startAirTimer(seconds: number): void {
+  private startAirTimer(end: number): void {
     const timerInterval = interval(50);
     const subone = timerInterval.subscribe((sec) => {
-      this.airProgressbarValue = 100 - (sec * 100) / seconds;
+      this.airProgressbarValue.next(100 - (sec * 100) / end);
       this.secInAir = sec;
 
-      this.timelineStepperSetter(this.airTimeline, seconds, this.secInAir, this.stepperAir)
+      this.timelineStepperSetter(this.airTimeline, end, this.secInAir, this.stepperAir);
 
-      if (this.secInAir === seconds) {
+      if (this.secInAir === end) {
         subone.unsubscribe();
       }
     });
   }
 
-  public startGroundTimer(begin: number, end: number): void {
+  private startGroundTimer(begin: number, end: number): void {
     const timer$ = interval(50);
-    let wasCausedDoubleEvent = false;
     const sub = timer$.subscribe((sec) => {
-      this.groundProgressbarValue = 100 - (sec * 100) / begin;
+      this.groundProgressbarValue.next(100 - (sec * 100) / begin);
       this.secTillLiftoff = sec;
 
-      this.timelineStepperSetter(this.groundTimeline, begin, this.secTillLiftoff, this.stepperGround)
+      this.timelineStepperSetter(this.groundTimeline, begin, this.secTillLiftoff, this.stepperGround);
 
       if (this.secTillLiftoff === begin) {
         // Object.keys(this.groundTimeline).reduce((accumulator, key) => {
@@ -140,28 +125,32 @@ export class LaunchPageComponent implements OnInit {
     });
   }
 
-  public capitalizeHeader(event:string): string {
+  public capitalizeHeader(event: string): string {
     let header = event.replace(/_/g, ' ');
     return header[0].toUpperCase() + header.slice(1).toLowerCase();
   }
 
-  public buildTimeline(timeline: {}):void {
+  public buildTimeline(timeline: {}): void {
     let timelineArray = Object.entries(timeline).map(([event, time]) => ({ event, time }));
     // @ts-ignore
-    timelineArray.sort((a,b) => a.time - b.time);
+    timelineArray.sort((a, b) => a.time - b.time);
 
-    this.groundTimeline = timelineArray.filter(element => element.time as number < 0).map(item => ({
-      seconds: item.time as number * (-1),
-      header: this.capitalizeHeader(item.event),
-      isVisible: false,
-      more: '~ ' + (item.time as number) * -1 + ' s. till liftoff',
-    }));
+    this.groundTimeline = timelineArray
+      .filter((element) => (element.time as number) < 0)
+      .map((item) => ({
+        seconds: (item.time as number) * -1,
+        header: this.capitalizeHeader(item.event),
+        isVisible: false,
+        more: '~ ' + (item.time as number) * -1 + ' s. till liftoff',
+      }));
 
-    this.airTimeline = timelineArray.filter(element => element.time as number > 0).map(item => ({
-      seconds: item.time as number,
-      header: this.capitalizeHeader(item.event),
-      isVisible: false,
-    }));
+    this.airTimeline = timelineArray
+      .filter((element) => (element.time as number) > 0)
+      .map((item) => ({
+        seconds: item.time as number,
+        header: this.capitalizeHeader(item.event),
+        isVisible: false,
+      }));
   }
 
   ngOnInit(): void {
@@ -178,7 +167,10 @@ export class LaunchPageComponent implements OnInit {
         }
         launch.links.flickr_images.length ? this.imagesToArray(launch.links.flickr_images) : null;
 
-        this.buildTimeline(launch.timeline);
+        if (Object.keys(launch.timeline).length > 2) {
+          this.containTimeLine = true;
+          this.buildTimeline(launch.timeline);
+        }
 
         const min = Math.max.apply(
           Math,
