178d32e35a70c49d0a70e411dd788aad68f2d9ee
Mariusz Briegmann <mariusz.briegmann@britenet.com.pl>
2022-08-25 10:45:25 +0200

    [BS-04][BS-07] Display Launches in Ships

--- src/app/componenets/ship-page/ship-page.component.ts
+++ src/app/componenets/ship-page/ship-page.component.ts
@@ -4,6 +4,10 @@ import { HttpClient } from '@angular/common/http';
 import { SpinnerService } from '../../services/spinner/spinner.service';
 import { IShip } from '../../interfaces/ships';
 import { DataService } from '../../services/data/data.service';
+import { map } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
+import { ILaunches } from '../../interfaces/launches';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-ship-page',
@@ -15,6 +19,15 @@ export class ShipPageComponent implements OnInit {
 
   public test: boolean = true;
 
+  public ALaunches: ILaunches[];
+  public Launches$: any[];
+
+  // Pagination
+  public pageLength: number = 0;
+  public pageSize: number = 3;
+  public pageSizeOptions: number[] = [this.pageSize, this.pageSize + 3];
+  public pageEvent: PageEvent | undefined;
+
   constructor(
     public spinnerService: SpinnerService,
     private route: ActivatedRoute,
@@ -25,13 +38,29 @@ export class ShipPageComponent implements OnInit {
   public getShip(ship_id: string) {
     this.dataService.loadShip(ship_id).subscribe((ship) => {
       this.ship = ship;
+      console.log(ship)
+
+      this.Launches$ = ship.missions.map(element => {
+        return this.dataService.loadLaunch(element.flight).pipe(map(flightId => flightId));
+      });
+
+      forkJoin(this.Launches$).subscribe( flightId => {
+        this.Launches$ = flightId; //data will be structured as [res[0], res[1], ...]
+        this.ALaunches = this.Launches$.slice((0 + 1 - 1) * this.pageSize).slice(0, this.pageSize);
+        console.log(this.Launches$.length)
+        this.pageLength = this.Launches$.length;
+      });
     });
+  }
+
+  public OnPaginate(event: PageEvent): void {
+    const offset = (event.pageIndex + 1 - 1) * event.pageSize;
+    this.ALaunches = this.Launches$.slice(offset).slice(0, event.pageSize);
   }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const shipId = routeParams.get('ship_id') as string;
-
     this.getShip(shipId);
   }
 }
